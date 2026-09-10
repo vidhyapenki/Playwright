965afebb108f106e9af2ca54e94e06033b9a5e0f
vidhyapenki <vidhya.penki@gmail.com>
2026-09-10 07:36:36 +0200

    updated

---
+++ tests/TC04playwrightlocators2.spec.js
@@ -0,0 +1,24 @@
+// Xpath
+// Different Type of Xpath
+// Relative XPATH
+// Absolute XPATH
+// /XPATH
+// //XPATH
+//  //a[@title="Try for free"]
+// a - Tag
+// title - Attribute
+// Try for free - Value
+
+
+
+//CSS - Cascading Style Sheet
+
+// Way to Write CSS
+// 1. If you know the class name - Then the CSS would be - .classname
+// 2. If you know the ID - Then the CSS would be - #id
+// 3. If you know the tag name - Then the CSS would be - tagname
+// 4. If you know the attribute - Then the CSS would be - [attribute="value"]  
+// 5. If you know the attribute and value - Then the CSS would be - tagname[attribute="value"] 
+
+// await page.getByRole('textbox', { name: 'First Name' })
+// await page.locator('button:has-text("Submit")')
